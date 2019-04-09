99bc258abe1bcea279bea9d1622494626bc849d0
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-04-09 22:35:36 +0300

    Single quotes instead of double quotes.

--- django-rename.py
+++ django-rename.py
@@ -15,21 +15,21 @@ def update_project_files(path, old_name, new_name):
     :param old_name: The old project name
     :param new_name: The new project name
     """
-    manage_path = os.path.join(path, "manage.py")
+    manage_path = os.path.join(path, 'manage.py')
     if not os.path.exists(manage_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), manage_path)
 
-    settings_path = os.path.join(path, old_name, "settings.py")
+    settings_path = os.path.join(path, old_name, 'settings.py')
     if not os.path.exists(settings_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), settings_path)
 
-    wsgi_path = os.path.join(path, old_name, "wsgi.py")
+    wsgi_path = os.path.join(path, old_name, 'wsgi.py')
     if not os.path.exists(wsgi_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), wsgi_path)
 
     files = [manage_path, settings_path, wsgi_path]
     for filename in files:
-        with FileInput(filename, inplace=True, backup=".bak") as f:
+        with FileInput(filename, inplace=True, backup='.bak') as f:
             for line in f:
                 sys.stdout.write(line.replace(old_name, new_name))
 
@@ -44,8 +44,8 @@ def get_project_name(path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("project_folder", type=str)
-    parser.add_argument("name", type=str)
+    parser.add_argument('project_folder', type=str)
+    parser.add_argument('name', type=str)
     args = parser.parse_args()
 
     project_name = get_project_name(args.project_folder)
